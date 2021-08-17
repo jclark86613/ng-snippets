91354578fbcf77531eba8007f72cd57daa9fa0dc
Jamie Clark <jamieclark333@hotmail.co.uk>
2021-08-18 00:44:42 +0100

    find .component.ts file list

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,59 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {Dirent, promises as fs, StatsBase} from 'fs';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-	
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "ng-component-template-snippet" is now active!');
+const fileSuffix = '.component.ts';
+const output = vscode.window.createOutputChannel("output");
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('ng-component-template-snippet.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from ng-component-template-snippet!');
-	});
-
+export async function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand(
+		'ng-component-template-snippet.scan',
+		createSnippets
+	);
 	context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 export function deactivate() {}
+
+let createSnippets = async (): Promise<void> => {
+
+	// fetch all .component.ts files
+	output.appendLine('Complete File List:');
+	let files = await getFileList();
+
+	// read file and create snipped json
+	files.forEach(file => {
+		output.appendLine('    ' + file.name);
+	});
+};
+
+async function getFileList(): Promise<Dirent[]> {
+	const workspace = vscode.workspace.workspaceFolders;
+	const uri = (workspace && workspace[0].uri.fsPath) || '';
+	const files = await recursiveGetTsFiles(uri + '/src');
+	return files;
+}
+
+async function recursiveGetTsFiles(uri:string): Promise<Dirent[]> {
+	//output list
+	const fileList: Dirent[] = [];
+
+	// all files in current uri
+    const files: Dirent[] = await fs.readdir(
+		uri,
+		{ withFileTypes: true }
+	);
+
+	// find all .component.ts files or read next dir
+	for( let file of files ) {
+		if (file.isFile() && file.name.slice(-fileSuffix.length) === fileSuffix) {
+			fileList.push(file);
+		}
+		if (file.isDirectory()) {
+			fileList.push( ... await recursiveGetTsFiles(uri + '/' + file.name) );
+		}
+	}
+
+	return fileList;
+}
